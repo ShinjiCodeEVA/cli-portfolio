03c3361722a1d718ee736c80427b39890d8e7964
harvie <solisharvie1@gmail.com>
2024-04-23 23:46:02 +0800

    favicon changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { TerminalBox } from "./components/templates/TerminalBox"
 import { Notice } from "./components/templates/Notice"
+import image from '../public/logo.png'
+import { CustomFavicon } from "./util/favicon/custom-favicon"
 
 function App() {
 
   return (
     <>
+      <CustomFavicon src={image}/>
      <div className="h-screen flex flex-col items-center justify-center">
        <Notice/>
        <TerminalBox />
---
+++ src/util/favicon/custom-favicon.tsx
@@ -0,0 +1,9 @@
+interface FaviconProps {
+    src: string; 
+  }
+  
+  export const CustomFavicon = ({ src }: FaviconProps) => {
+    return (
+      <link rel="icon" type="image/x-icon" href={src} />
+    );
+  };
